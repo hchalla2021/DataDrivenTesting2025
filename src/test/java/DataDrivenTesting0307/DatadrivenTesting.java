7afc0a1ef8a7a48756899fbeb192a763e491737c
hchalla2021 <hchalla2020@gmail.com>
2025-03-08 12:49:57 +0530

    added new DD Testing framework
    added new DD Testing framework

---
+++ src/test/java/DataDrivenTesting0307/DatadrivenTesting.java
@@ -0,0 +1,8 @@
+package DataDrivenTesting0307;
+import org.testng.annotations.Test;
+public class DatadrivenTesting {
+    @Test
+  public  void DDTest() {
+        System.out.println("Hello123");
+  }
+}
---
+++ src/test/java/excel/Xls_Reader.java
@@ -0,0 +1,7 @@
+package excel;
+
+public class Xls_Reader {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
